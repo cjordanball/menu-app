19c1dcc027e540345bbfe10c0beac6d0e0a43d8e
Jordan Ball <cjordanball@ballwebdev.com>
2024-07-26 10:15:21 -0400

    add sqllite db and content for meals grid

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -1,3 +1,30 @@
-export default function Meals() {
-	return <h1>Meals Page</h1>;
+import Link from 'next/link';
+import styles from './page.module.css';
+import { MealsGrid } from '@/components/meals/meals-grid';
+import { getMeals } from '@/lib/meals';
+
+export default async function Meals() {
+	const meals = await getMeals();
+
+	return (
+		<>
+			<header className={styles.header}>
+				<h1>
+					Delicious meals, created{' '}
+					<span className={styles.highlight}>by you</span>
+				</h1>
+				<p>
+					Choose your favorite recipe and cook it yourself. It is easy and fun!
+				</p>
+				<p className={styles.cta}>
+					<Link className={`undefined`} href='/meals/share'>
+						Share Your Favorite Recipe
+					</Link>
+				</p>
+			</header>
+			<main className={styles.main}>
+				<MealsGrid meals={meals} />
+			</main>
+		</>
+	);
 }
